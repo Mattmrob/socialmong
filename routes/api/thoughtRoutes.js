fdda587a9667f206d9a28697724d6d992b55cc68
Mysmelf <matthewr9937@gmail.com>
2023-12-14 12:19:48 -0800

    Created initial routing layout and placeholders for gets / pulls / deletes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+// import thought controllers here
+
+// router.route('/')
+// .get("This is where getAllThoughts would go")
+// .post("This is where createNewThought would go - ID MUST BE ADDED TO ASSOCIATED USER");
+
+// router.route('/:thoughtId')
+//   .get("This is where getThoughtById would go")
+//   .put("This is where updateThoughtById would go")
+//   .delete("This is where deleteThoughtById would go");
+
+//   router.route('/:thoughtId/reactions')
+//   .post("This is where createReaction would go - STORED IN A THOUGHT'S ARRAY")
+//   .delete("This is where deleteReaction would go - target reaction Id value");
+
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+// Import user controllers here
+
+// use user controllers here
+// router.route('/')
+// .get("this is where getAllUsers would go")
+// .post("this is where createNewUser would go");
+
+// router.route('/:userId').get("this is where getSingleUserById would go");
+
+// router.route('/:userId/friends/:friendId')
+// .get("this is where addNewFriend would go")
+// .delete("this is where removeFriend would go");
+
+module.exports = router;
