5b50a281fa17b9258c28595dddb6db84137239b7
Mysmelf <matthewr9937@gmail.com>
2023-12-16 14:44:21 -0800

    Created friends list add and delete controllets, need to import and create routes for testing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,27 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
+    
+    // --------- USERS ---------
+
     // Get all users
     async getUsers(req, res) {
-      try {
+        try {
         const users = await User.find();
         res.json(users);
-      } catch (err) {
+        } catch (err) {
         res.status(500).json(err);
-      }
+        }
     },
 
     // create a new user
     async createUser(req, res) {
-      try {
+        try {
         const user = await User.create(req.body);
         res.json(user);
-      } catch (err) {
+        } catch (err) {
         res.status(500).json(err);
-      }
+        }
     },
 
     // Get a single user
@@ -40,38 +43,84 @@ module.exports = {
     // update single user
     async updateUser(req, res) {
         try {
-          const user = await User.findOneAndUpdate(
+            const user = await User.findOneAndUpdate(
             { _id: req.params.userId },
             { $set: req.body },
             { runValidators: true, new: true }
-          );
-    
-          if (!user) {
-            return res.status(404).json({ message: 'No application with this id!' });
-          }
-    
-          res.json(user);
-        } catch (err) {
-          console.log(err);
-          res.status(500).json(err);
-        }
-      },
+            );
 
-    
+            if (!user) {
+            return res.status(404).json({ message: 'No application with this id!' });
+            }
+
+            res.json(user);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+        },
 
     // Delete a user and associated apps
     async deleteUser(req, res) {
-      try {
+        try {
         const user = await User.findOneAndDelete({ _id: req.params.userId });
-  
+
         if (!user) {
-          return res.status(404).json({ message: 'No user with that ID' });
+            return res.status(404).json({ message: 'No user with that ID' });
         }
-  
+
         await Application.deleteMany({ _id: { $in: user.applications } });
         res.json({ message: 'User and associated apps deleted!' })
-      } catch (err) {
+        } catch (err) {
         res.status(500).json(err);
-      }
+        }
     },
-  };
+
+    // --------- FRIENDS LIST ---------
+
+    // Add a friend to a user
+    async addFriend(req, res) {
+
+        try {
+        const user = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.params.friendId } },
+            { runValidators: true, new: true }
+        );
+
+        if (!user) {
+            return res
+            .status(404)
+            .json({ message: 'No user found with that ID' });
+        }
+
+        res.json(user);
+        } catch (err) {
+        res.status(500).json(err);
+        }
+    },
+
+    // Remove a friend to a user
+    async removeFriend(req, res) {
+        try {
+        const user = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: { _id: req.params.friendId } } },
+            { runValidators: true, new: true }
+        );
+
+        if (!user) {
+            return res
+            .status(404)
+            .json({ message: 'No student found with that ID' });
+        }
+
+        res.json(user);
+        } catch (err) {
+        res.status(500).json(err);
+        }
+    },
+};
+
+  
+
